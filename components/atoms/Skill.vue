5c608438f9391fdab89853565c8eb358e0db46b0
Toshiharu Nakayama <nakayama@solidcom.co.jp>
2020-03-23 13:52:21 +0900

    スタイルの修正

--- components/atoms/Skill.vue
+++ components/atoms/Skill.vue
@@ -1,8 +1,7 @@
 <template>
   <v-card>
-    <v-card-title class="headline">{{ item.fields.name }}</v-card-title>
-    <v-card-text>Level : {{ item.fields.level }}</v-card-text>
-    <div class="pa-6 mt-n7">
+    <v-card-title class="title">{{ item.fields.name }}</v-card-title>
+    <div class="pa-10 mt-n7">
       <Progress :level="item.fields.level" />
     </div>
   </v-card>
